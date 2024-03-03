4e63f3757a4b5633c0287c2cedbda92d50cb5280
dihanp <149501329+dihanp@users.noreply.github.com>
2024-03-03 03:03:35 +0100

    Add files via upload

---
+++ uers.js
@@ -0,0 +1,18 @@
+[
+    {
+        "id": 1,
+        "Name":""pubudu"
+        "email": "pubudu@gmail.com"
+    },
+    {
+        "id": 2,
+        "Name": "devi",
+        "email": "devi@gmail.com"
+    },
+    {
+        "id": 3,
+        "Name": "johan",
+        "email": "johan@gmail.com"
+    }
+
+  ]
